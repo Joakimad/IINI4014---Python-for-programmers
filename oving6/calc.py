6832655d58a8335f9b8181a18bae94c0cc67bfe5
Joakim Moe Adolfsen <joakimad@stud.ntnu.no>
2020-04-19 14:29:49 +0200

    o6 done

---
+++ oving6/calc.py
@@ -0,0 +1,9 @@
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+
+while 1 == 1:
+    num1 = 1
+    num2 = {{int(open("flag.txt", "r").read())}}
+    print(num1, "+", num2, "=", add(num1, num2))
